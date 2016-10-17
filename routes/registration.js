5576f07abce7fab8010d3e82b74d5c2f5c5bd800
Mukul Jain <jainmukul1996@gmail.com>
2016-10-18 03:50:43 +0530

    roll no. checker on step 1 of sign up

--- routes/registration.js
+++ routes/registration.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var User = require('../models/user');
 
 router.post('/register', passport.authenticate('local-signup', {
    successRedirect : '../response/true', // redirect to the secure profile section
@@ -14,5 +15,24 @@ router.post('/login', passport.authenticate('local-login', {
    failureRedirect : '../response/false', // redirect back to the signup page if there is an error
    failureFlash : true // allow flash messages
 }));
+
+router.post('/rollno', function(req, res, next) {
+  console.log(req.body);
+
+  User.findOne({ 'rollNo' :  rollNo }, function(err, user) {
+      // if there are any errors, return the error
+      if (err){
+          return done(err);
+        }
+      // check to see if theres already a user with that email
+      if (user) {
+        res.end("{response : false}");
+      }
+    });
+
+    res.end("{response : true}");
+
+});
+
 
 module.exports = router;
