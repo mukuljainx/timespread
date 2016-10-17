87fe4ada93fe56f81882a65c50876aead67f5c81
Mukul Jain <jainmukul1996@gmail.com>
2016-10-18 02:16:25 +0530

    true spelling correction :P

--- routes/response.js
+++ routes/response.js
@@ -7,14 +7,11 @@ var bodyParser = require('body-parser');
 // true
 router.get('/true', function(req, res, next) {
   console.log(req);
-  res.end("{response : ture}");
+  res.end("{response : true}");
 });
 
 // true
 router.get('/false', function(req, res, next) {
-  var response = "";
-  console.log('response is ::: ' + req.session.flash.Message[0] )
-  req.session.flash.Message[0] == 'rollNo already exists' ? response = "taken" : response = "false";
   res.end("{response : " + req.session.flash.Message[0] + "}");
 });
 
