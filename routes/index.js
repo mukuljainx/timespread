81530ce5700b9d0a2db6791460db46faac9be1c2
Mukul Jain <jainmukul1996@gmail.com>
2016-10-25 19:06:15 +0530

    added terms & condtion fake page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Time Spread' });
+});
+
+router.get('/termsandconditions', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../public/terms.html'));
 });
 
 module.exports = router;
